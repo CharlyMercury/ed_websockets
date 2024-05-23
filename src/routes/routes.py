c4392f4fcb945d270a81b23ce7610e8a54cb52f0
Carlos Tovar <carlos.mercury92@gmail.com>
2024-05-23 14:30:04 -0600

    Comment aws client x3

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -57,7 +57,7 @@ async def get_status(plantel: str, modulo: int):
         "plantel": plantel,
         "modulo": f"modulo{modulo}",
     }
-    mqtt_client.publish_to_topics(topic_pub_="status/", input_message=message_status)
+    # mqtt_client.publish_to_topics(topic_pub_="status/", input_message=message_status)
     return {"prepa1": "Successfully"}
 
 
@@ -67,7 +67,7 @@ async def get_update_db(plantel: str, modulo: int):
         "plantel": plantel,
         "modulo": f"modulo{modulo}",
     }
-    mqtt_client.publish_to_topics(topic_pub_="update/db", input_message=message_status)
+    # mqtt_client.publish_to_topics(topic_pub_="update/db", input_message=message_status)
     return {"prepa1": "Successfully"}
 
 
@@ -77,7 +77,7 @@ async def get_update_esp32(plantel: str, modulo: int):
         "plantel": plantel,
         "modulo": f"modulo{modulo}",
     }
-    mqtt_client.publish_to_topics(topic_pub_="update/esp32", input_message=message_status)
+    # mqtt_client.publish_to_topics(topic_pub_="update/esp32", input_message=message_status)
     return {"prepa1": "Successfully"}
 
 
